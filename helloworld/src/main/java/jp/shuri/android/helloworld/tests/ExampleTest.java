8b8df6e5713f29b3d930d143e1fb0c383387ac35
YAMANE Toshiaki <yamanetoshi@gmail.com>
2014-02-02 21:43:33 +0900

    Fix tests

--- helloworld/src/main/java/jp/shuri/android/helloworld/tests/ExampleTest.java
+++ helloworld/src/main/java/jp/shuri/android/helloworld/tests/ExampleTest.java
@@ -1,14 +1,33 @@
 package jp.shuri.android.helloworld.tests;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.test.InstrumentationTestCase;
+import android.widget.TextView;
+
+import jp.shuri.android.helloworld.MainActivity;
+import jp.shuri.android.helloworld.R;
 
 /**
  * Created by rms on 2/1/14.
  */
 public class ExampleTest extends InstrumentationTestCase {
+
+    private MainActivity mCurrentActivity;
+    Instrumentation mInstrumentation;
+    private Instrumentation.ActivityMonitor mMonitor;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        mInstrumentation = getInstrumentation();
+        mMonitor = mInstrumentation.addMonitor(MainActivity.class.getName(), null, false);
+        mCurrentActivity = (MainActivity)mInstrumentation.waitForMonitorWithTimeout(mMonitor, 5);
+    }
+
     public void testExample() throws Exception {
-        final int expected = 1;
-        final int reality = 5;
-        assertEquals(expected, reality);
+        TextView textView = (TextView)mCurrentActivity.findViewById(R.id.hello_world);
+        assertEquals("Hello World!", textView.getText().toString());
     }
 }
